51f521b4a6dbddad8ca58e0775451816f732d142
DevSaalih <soliualley@gmail.com>
2022-06-06 04:23:32 +0100

    update

---
+++ medium-build/schemas/comment.js
@@ -0,0 +1,33 @@
+export default {
+  name: 'comment',
+  title: 'Comment',
+  type: 'document',
+  fields: [
+    {
+      name: 'name',
+      title: 'Name',
+      type: 'string',
+    },
+    {
+      name: 'approved',
+      title: 'Approved',
+      type: 'boolean',
+      description: "Comment won't show on the site without approval",
+    },
+    {
+      name: 'email',
+      title: 'Email',
+      type: 'string',
+    },
+    {
+      name: 'comment',
+      title: 'Comment',
+      type: 'text',
+    },
+    {
+      name: 'post',
+      type: 'reference',
+      to: { type: 'post' },
+    },
+  ],
+}
